f1e13b54d2a8434298ac741e5532ef5f905076ab
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-03-11 23:15:55 +0000

    chore(p4): add argument validation

---
+++ part_four/train.py
@@ -0,0 +1,17 @@
+#!.venv/bin/python
+import sys
+from pathlib import Path
+from src.parse_folder import parse_folder
+
+
+def main():
+    root = parse_folder(sys.argv)
+    
+
+
+
+
+    
+
+if __name__ == "__main__":
+    main()
