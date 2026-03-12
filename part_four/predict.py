7c8969b09570ec0963c9c62e9f59ff7c758e8e3a
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-03-12 21:56:13 +0000

    chore: plot predictions results

--- part_four/predict.py
+++ part_four/predict.py
@@ -4,7 +4,12 @@ import sys
 from src.train.dataloaders import transform
 from src.model.cnn import CNN
 from PIL import Image
+import matplotlib.pyplot as plt
+from pathlib import Path
+import plantcv.plantcv as pcv
 
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from part_three.utils.transform_utils import get_image_transformations
 
 def main():
     # prepare image
@@ -13,12 +18,13 @@ def main():
         sys.exit(1)
 
     try:
-        img = Image.open(sys.argv[1]).convert('RGB')
-    except Exception as e:
+        img_path = Path(sys.argv[1])
+        img_raw = Image.open(sys.argv[1]).convert('RGB')
+    except:
         print("Cannot open image", file=sys.stderr)
         sys.exit(1)
 
-    img = transform(img).unsqueeze(dim=0)
+    img = transform(img_raw).unsqueeze(dim=0)
 
     # load model
     checkpoint = torch.load('./model.pth')
@@ -30,8 +36,31 @@ def main():
     label = model(img)
     label = torch.argmax(label, dim=1).item()
     label = classes[label]
+    actual_label = img_path.parent.name
 
-    print(label)
+
+    # plot result
+    transformed_img = get_image_transformations(
+        pcv.readimage(img_path)[0],
+        mode='plot'
+    )['mask']
+
+    plt.style.use('dark_background')
+    fig, (ax1, ax2) = plt.subplots(1, 2) 
+    fig.suptitle(
+        f"Predicted: {label}\nActual: {actual_label}",
+        color="green" if actual_label==label else "red",
+        fontsize=16,
+        fontweight="bold",
+    )
+    ax1.imshow(img_raw)
+    ax1.axis('off')
+    ax2.imshow(transformed_img)
+    ax2.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
+    plt.close()
 
 if __name__ == "__main__":
     main()
