b39e3795ed4e90e77a0275710a9c48ab5a956245
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-03-14 18:24:23 +0000

    chore: add dataset installer

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+
+curl -L -o leafs.zip https://cdn.intra.42.fr/document/document/43409/leaves.zip
+unzip leafs.zip
+rm -rf leafs.zip
